c7415d19b5869300b02579bc598a40b5b6be332e
rklpoi5678 <rklpoi5678@naver.com>
2025-06-04 00:57:22 +0900

    UP: BuG fix

--- apps/guguTravel/app.config.js
+++ apps/guguTravel/app.config.js
@@ -1,15 +1,39 @@
+import type { ExpoConfig, ConfigContext } from 'expo/config';
 
-module.exports = {
-  version: "1.0.2",
-  config: {
-    googleMaps: {apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY},
+const VERSION_NAME = '1.0.1';
+const VERSION_CODE = 1000001;
+
+const AND_PACKAGE = "com.youngikim.appsgugutravel"
+const IOS_PACKAGE = "com.youngikim.appsgugutravel"
+
+const GOOGLE_MAPS = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL
+const SUPABASE__ANON_URL = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
+const EAS = process.env.EXPO_PUBLIC_EAS_PROJECT_ID
+const GOOGLE_ADS = process.env.EXPO_PUBLIC_GOOGLE_MOBILE_ADS_APP_ID
+
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  return{
+    ...config,
+  version: VERSION_NAME,
+
+  android:{
+    package: AND_PACKAGE,
+    versionCode: VERSION_CODE,
+    config: {
+    googleMaps: {apiKey: GOOGLE_MAPS},
   },
-  android:{package: "com.youngikim.appsgugutravel"},
-  ios:{bundleIdentifier: "com.youngikim.appsgugutravel"},
+  },
+  ios:{
+    bundleIdentifier: IOS_PACKAGE,
+    buildNumber: VERSION_CODE + '',
+  },
   extra: {
-    supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
-    supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-    eas: {projectId: process.env.EXPO_PUBLIC_EAS_PROJECT_ID},
-    googleAds: {android_app_id: process.env.EXPO_PUBLIC_GOOGLE_MOBILE_ADS_APP_ID},
+    supabaseUrl: SUPABASE_URL,
+    supabaseAnonKey: SUPABASE__ANON_URL,
+    eas: {projectId: EAS },
+    googleAds: {android_app_id: GOOGLE_ADS },
     },
+  }
 }
